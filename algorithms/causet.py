Fixed MDS to have most significant directions as earlier indices, not later. Added notebook for example causal set MDS

algorithms/causet.py
@@ -1,6 +1,7 @@
-""" Calculate Minkowski separations from adjacency matrix of a causal set or DAG"""
+""" Create adjacency matrices for causal sets"""
 import numpy as np
 import metrics
+import dagsep   
    
 def causet_adj_matrix(S, R):
     """ Return causal set adjacency matrix A
@@ -18,22 +19,37 @@ def causet_adj_matrix(S, R):
     return A  
 
 def minkowski_causet(N, D, fix_ends=True):
-    """ Return N point, D dimensional Minkowski scattered causet adj-matrix"""
+    """ Return N point, D dimensional Minkowski scattered causet adj-matrix
+        A - adjacency matrix     NxN
+        R - original coordinates NxD"""
     import scatter
     import dagsep
     R = scatter.minkowski_interval(N, D, fix_ends)
     S = metrics.sq_separations(R, metrics.minkowski)
     A = causet_adj_matrix(S, R)
-    return A     
+    return A, R     
     
 def de_sitter_causet(N, D, eta_0, eta_1):
-    """ Return N point, D dimensional de Sitter scattered causet adj-matrix"""
+    """ Return N point, D dimensional de Sitter scattered causet adj-matrix
+        A - adjacency matrix     NxN
+        R - original coordinates NxD"""
     import scatter
     import dagsep
     R = scatter.de_sitter_interval(N, D, eta_0, eta_1)
     S = metrics.sq_separations(R, metrics.de_sitter)
     A = causet_adj_matrix(S, R)
-    return A       
+    return A, R       
+
+def random_dag(N, k, tc=True):
+    p = (1. * k)/N
+    A = np.random.random((N,N))
+    A[A>(1-p)] = 1
+    A[A<(1-p)] = 0
+    for i in range(N):
+        A[i,i:] = 0
+    if tc:
+        A = dagsep.transitive_completion(A)
+    return A
 
 if __name__ == "__main__":
     print __doc__

algorithms/dagsep.py
@@ -63,6 +63,8 @@ def longest_path_matrix(A, dmax=None):
          Lorentzian spacetimes but this is not proven to my knowledge 
     """
     N = A.shape[0]
+    if dmax is None:
+        dmax = N
     LP = np.zeros((N, N))
     i = 1
     B = A[:,:]
@@ -72,7 +74,7 @@ def longest_path_matrix(A, dmax=None):
         LP = np.maximum.reduce((LP, path_length))
         B = np.dot(B, A)
         i += 1
-        if dmax and (i == dmax):
+        if i == dmax:
             return LP
     return LP
     

algorithms/mds.py
@@ -45,8 +45,8 @@ def m_eval(E, U, D):
     Returns:
     E_max -- Diagonal DxD array of sqrt of eigenvalues
     U_max -- NxD array of corresponding eigenvectors"""
-    E_max = np.diag(np.concatenate((E[0:1]*-1., E[-1*(D-1):])))
-    U_max = np.concatenate((U[:, 0:1], U[:, -1*(D-1):]), axis=1)
+    E_max = np.diag(np.concatenate((E[:1]*-1., E[:-1*D:-1])))
+    U_max = np.concatenate((U[:, :1], U[:, :-1*D:-1]), axis=1)
     return E_max, U_max
 
 def MDS(ds2, D, method='euclidean'):

algorithms/metrics.py
@@ -9,7 +9,7 @@ Riemannian
 ################################################################################
 """
 def euclidean(x, y):
-    """Calculcate Euclidean distance between x and y """
+    """Calculate Euclidean distance between x and y """
     assert len(x)==len(y), 'ERROR - vectors in Euclidean metric have different lengths'
     if np.array_equal(x, y):
         return 0.
@@ -23,6 +23,9 @@ def spherical(x_, y_):
     assert len(x_)==len(y_), 'ERROR - vectors in spherical metric have different lengths'
     if np.array_equal(x_, y_):
         return 0.
+    if len(x_) == 1:
+        # special case can be calculated more quickly
+        return min(np.abs(x_[0] - y_[0]), (2.*np.pi - np.abs(x_[0] - y_[0])))
     x, y = angular_to_cartesian(x_), angular_to_cartesian(y_)
     cos_psi = np.dot(x,y)
     psi = np.arccos(cos_psi)
@@ -63,8 +66,10 @@ def hyperbolic(x, y, a=1.):
 Lorentzian
 ################################################################################
 """
-def minkowski(x, y):
-    """Calculate Minkowski separation between x and y using -++...+ convention"""
+def minkowski(x, y, c=1.):
+    """Calculate Minkowski separation between x and y using -++...+ convention
+    
+    c - speed of light - default to 1."""
     assert len(x)==len(y), 'ERROR - vectors in Minkowski metric have different lengths'
     if np.array_equal(x, y):
         return 0.
@@ -72,7 +77,7 @@ def minkowski(x, y):
     dt2 = dt * dt
     dx = np.array([x[i] - y[i] for i in range(1, len(x))])
     dx2 = dx * dx
-    dx2sum = sum(dx2)
+    dx2sum = sum(dx2) * c * c
     return dx2sum - dt2 
       
 def minkowski_periodic(x, y, L=[]):
@@ -122,11 +127,13 @@ def sq_separations(R, metric):
         c - speed of light"""
     S = np.zeros((R.shape[0], R.shape[0]))
     for i in range(R.shape[0]):
-        for j in range(R.shape[0]):
+        for j in range(i):
             if i == j:
                 S[i, j] = 0.    
             else:
-                S[i, j] = metric(R[i,:], R[j,:])
+                d = metric(R[i,:], R[j,:])
+                S[i, j] = d
+                S[j, i] = d
     return S
     
 if __name__ == "__main__":