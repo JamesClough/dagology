30cf1b8f0052e7efea75a8c6b208ebe565217d82
NicolasKozak <nicolas.kozak5@gmail.com>
2024-02-09 11:09:57 +0000

    Adding greedy paths (longest/shortest)

--- dagology/paths/paths.py
+++ dagology/paths/paths.py
@@ -13,3 +13,19 @@ def longest_path(node, weights, distances, predecessors):
         new_distance = distances[node] - w
         if distances[v] > new_distance:
             distances[v], predecessors[v] = new_distance, node
+
+def greedy_shortest_path(node, weights, distances, predecessors):
+    if weights:
+        min_neighbor = min(weights, key=weights.get)
+        new_distance = distances[node] + weights[min_neighbor]
+        if distances[min_neighbor] > new_distance:
+            distances[min_neighbor] = new_distance
+            predecessors[min_neighbor] = node
+
+def greedy_longest_path(node, weights, distances, predecessors):
+    if weights:
+        max_neighbor = max(weights, key=weights.get)
+        new_distance = distances[node] - weights[max_neighbor]
+        if distances[max_neighbor] > new_distance:
+            distances[max_neighbor] = new_distance
+            predecessors[max_neighbor] = node
